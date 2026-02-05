760b9f1cfa8c5e84e6b1fd3a01d901a13f3dff4f
Davi Vicente <davivicente.live@hotmail.com>
2026-02-05 15:56:38 -0300

    Add files via upload

---
+++ ex051.py
@@ -0,0 +1,6 @@
+print('Arithmetic Progression')
+f1 = int(input('Enter the \033[31mfirst term\033[m: '))
+r = int(input('Enter the \033[32mraison\033[m: '))
+for c in range(f1, f1 + (10 - 1) * r, r):
+    print('{} >'.format(c) , end=' ')
+print('END')
